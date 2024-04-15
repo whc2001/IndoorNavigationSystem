62611f97cba7d2edb44a4b1fb86f2e56346ef2b0
whc2001 <ianwang0122@outlook.com>
2024-04-15 14:16:54 -0400

    fix: use local path for result

--- Backend/src/main/java/yorku/indoor_navigation_system/backend/ImageCacheResourceHandler.java
+++ Backend/src/main/java/yorku/indoor_navigation_system/backend/ImageCacheResourceHandler.java
@@ -3,12 +3,13 @@ package yorku.indoor_navigation_system.backend;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import yorku.indoor_navigation_system.backend.utils.FileUtils;
 
 @Configuration
 public class ImageCacheResourceHandler implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/result/**")
-                .addResourceLocations("file:src/main/resources/static/result/");
+                .addResourceLocations("file:" + FileUtils.getStaticResPath() + "result/");
     }
 }
