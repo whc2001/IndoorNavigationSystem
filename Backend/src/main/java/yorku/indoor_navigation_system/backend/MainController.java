00c12b5cdd5aa1300c7121a6a781cdfb2231fc2c
whc2001 <ianwang0122@outlook.com>
2024-04-15 17:08:18 -0400

    fix: malformed result dir path

--- Backend/src/main/java/yorku/indoor_navigation_system/backend/MainController.java
+++ Backend/src/main/java/yorku/indoor_navigation_system/backend/MainController.java
@@ -194,6 +194,6 @@ public class MainController {
         if (s == null || e == null) {
             return null;
         }
-        return algorithm.Navigate(s, e, FileUtils.getStaticResPath() + "/result");
+        return algorithm.Navigate(s, e, FileUtils.getStaticResPath() + "result/");
     }
 }
