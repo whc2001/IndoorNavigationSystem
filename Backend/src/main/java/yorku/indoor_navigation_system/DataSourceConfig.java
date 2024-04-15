7c0e0aeaef75049ba21139870bc580eee1a6c41f
whc2001 <ianwang0122@outlook.com>
2024-04-15 16:46:56 -0400

    feat: use local path for db

--- Backend/src/main/java/yorku/indoor_navigation_system/DataSourceConfig.java
+++ Backend/src/main/java/yorku/indoor_navigation_system/DataSourceConfig.java
@@ -1,21 +1,27 @@
 package yorku.indoor_navigation_system;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.lang.NonNull;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
 
+    @Value("${application.db.path}")
+    @NonNull
+    private String dbPath;
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.sqlite.JDBC");
-        dataSource.setUrl("jdbc:sqlite:db/MainDatabase.db");
+        dataSource.setUrl("jdbc:sqlite:" + dbPath);
         return dataSource;
     }
 }
